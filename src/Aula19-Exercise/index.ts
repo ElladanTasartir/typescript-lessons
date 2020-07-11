ee62ed861a561c8688cf6672d032eb75c71bdee0
Erick Malta <erickmalta100@gmail.com>
2020-07-11 18:49:24 -0300

    feat: :rocket: Custom videoplayer created with TS, classes and interfaces

--- src/Aula19-Exercise/index.ts
+++ src/Aula19-Exercise/index.ts
@@ -1,3 +1,4 @@
 // Para instalar declaration files:
 // Colocamos @types/nome-da-lib, quando a lib em si já não vem com declaration files para os tipos
-import './form-control';
+// import './form-control';
+import '../Aula36 - Classes and Interfaces - Exercise/index';
---
+++ src/Aula36 - Classes and Interfaces - Exercise/index.ts
@@ -0,0 +1,55 @@
+interface VideoPlayerElements {
+  videoPlayer: HTMLVideoElement;
+  playButton: HTMLButtonElement;
+  stopButton: HTMLButtonElement;
+}
+
+interface VideoPlayerProtocols {
+  playToggle(): void;
+  stop(): void;
+  iniciarEventos(): void;
+}
+
+export default class VideoPlayer implements VideoPlayerProtocols {
+  private videoPlayer: HTMLVideoElement;
+  private playButton: HTMLButtonElement;
+  private stopButton: HTMLButtonElement;
+
+  constructor(videoPlayerelements: VideoPlayerElements) {
+    this.videoPlayer = videoPlayerelements.videoPlayer;
+    this.playButton = videoPlayerelements.playButton;
+    this.stopButton = videoPlayerelements.stopButton;
+    this.iniciarEventos();
+  }
+
+  playToggle(): void {
+    if (this.videoPlayer.paused) {
+      this.videoPlayer.play();
+      this.playButton.innerText = 'Pause';
+    } else {
+      this.videoPlayer.pause();
+      this.playButton.innerText = 'Play';
+    }
+  }
+  stop(): void {
+    this.videoPlayer.pause();
+    // Volta o vídeo para o 0
+    this.videoPlayer.currentTime = 0;
+    this.playButton.innerText = 'Play';
+  }
+  iniciarEventos(): void {
+    // Para que o this seja a classe
+    this.playButton.addEventListener('click', () => {
+      this.playToggle();
+    });
+    this.stopButton.addEventListener('click', () => {
+      this.stop();
+    });
+  }
+}
+
+const videoPlayer = new VideoPlayer({
+  videoPlayer: document.querySelector('.video') as HTMLVideoElement,
+  playButton: document.querySelector('.play') as HTMLButtonElement,
+  stopButton: document.querySelector('.stop') as HTMLButtonElement,
+});
