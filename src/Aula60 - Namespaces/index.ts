14c317aaf0a2f9d5dbb5ea7d276861f034a244d8
Erick Malta <erickmalta100@gmail.com>
2020-07-14 02:27:42 -0300

    docs: :pencil: Added importing namespace to TS file

--- src/Aula60 - Namespaces/index.ts
+++ src/Aula60 - Namespaces/index.ts
@@ -1,30 +1,15 @@
-// Namespaces são como um escopo a mais
-// Não é uma funcionalidade tão interessante em JS/TS, porque já existe o sistema de módulos
-// Que faz o serviço de uma forma ótima e inclusive o próprio ESLINT reclama quando utilizamos
-// Namespaces, porque além de não ser uma função existente no JS, substitui de forma desnecessária
-// Algo que já funciona perfeitamente
-/* eslint-disable @typescript-eslint/no-namespace */
-namespace MeuNamespace {
-  // Pode-se criar qualquer coisa dentro de um namespace, inclusive outros namespaces
-  export const nomeDoNamespace = 'Erick';
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+/// <reference path="module.ts" />
+// Sempre é o ideal usar o sistema de módulos do JS
+// O reference path é uma triple slash directive que recebe uma tag xml
+// Essa tag, referencia uma dependência do nosso módulo
+// Porém, isso não vai funcionar se estivermos usando no tsconfig commonJS ou ESM
+// Somente, AMD (biblioteca externa) ou System
+// E, também é necessário configurar para o TS compilar um outfile, porque
+// Para isso funcionar, tudo precisaria estar no mesmo arquivvo Js na compilação
+// Podemos também falar no comando de compilação que queremos apenas um arquivo de compilação
+// npx tsc diretorio/nomeDoArquivo.ts --outFile diretorio/nomeDoArquivo.js
+// Dessa forma, compilamos para o JS e conseguimos executar o bundle gerado
 
-  export class PessoaDoNamespace {
-    constructor(public nome: string) {}
-  }
-
-  const pessoa = new PessoaDoNamespace('Erick');
-  console.log(pessoa);
-
-  export namespace OutroNamespace {
-    export const nomeDoNamespace = 'Nome no outro namespace';
-  }
-}
-// É impossível de se acessar o que está dentro do namespace de fora, sem exportar os dados
-// E para pegar essas informações vindas do namespace, podemos chamá-lo e capturar as informações
-// Que exportamos de lá
-const pessoaDoNamespace = new MeuNamespace.PessoaDoNamespace('Erick');
-console.log(pessoaDoNamespace);
 console.log(MeuNamespace.nomeDoNamespace);
 console.log(MeuNamespace.OutroNamespace.nomeDoNamespace);
-
-export default 1;
